3fc40e8c499c326c63b12196f80273083104bf10
釜野由里佳 <yrkmn19@gmail.com>
2018-02-05 01:59:38 +0900

    Initial Commit

---
+++ Gacha/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Gacha
+//
+//  Created by 釜野由里佳 on 2018/02/04.
+//  Copyright © 2018年 釜野由里佳. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
